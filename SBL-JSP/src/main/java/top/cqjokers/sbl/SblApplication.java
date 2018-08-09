aeea3fb23451d34dfa4bef758587072c40503127
cqjokers <517552494@qq.com>
2018-08-09 16:19:54 +0800

    (二)JSP的支持

---
+++ SBL-JSP/src/main/java/top/cqjokers/sbl/SblApplication.java
@@ -0,0 +1,14 @@
+package top.cqjokers.sbl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SblApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SblApplication.class, args);
+    }
+}
